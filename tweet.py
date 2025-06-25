a550c535583525d19f27d2335d83bcdbed368cea
Aballard92 <alexballard92@hotmail.co.uk>
2025-06-25 21:10:31 +0100

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,26 +1,25 @@
 import csv, datetime, os, pathlib
 import tweepy
 
-# --- Auth setup ---
-auth = tweepy.OAuth1UserHandler(
-    os.environ["API_KEY"],
-    os.environ["API_SECRET"],
-    os.environ["ACCESS_TOKEN"],
-    os.environ["ACCESS_SECRET"],
+# ----------------- OAuth 2.0 (user context) -----------------
+client = tweepy.Client(
+    bearer_token=os.environ["BEARER_TOKEN"],
+    consumer_key=os.environ["API_KEY"],
+    consumer_secret=os.environ["API_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_SECRET"],
+    wait_on_rate_limit=True,
 )
-api = tweepy.API(auth)
 
-# --- Load the quotes ---
+# ----------------- pick today's quote -----------------
 root = pathlib.Path(__file__).parent
 with open(root / "quotes.csv", newline="", encoding="utf-8") as f:
     quotes = [row[0] for row in csv.reader(f) if row]
 
-# --- Pick quote of the day ---
 start = datetime.date(2025, 1, 1)
-today = datetime.date.today()
-idx = (today - start).days % len(quotes)
+idx = (datetime.date.today() - start).days % len(quotes)
 quote = quotes[idx]
 
-# --- Post to X ---
-api.update_status(status=quote)
+# ----------------- post -----------------
+client.create_tweet(text=quote)
 print("Tweeted:", quote)
